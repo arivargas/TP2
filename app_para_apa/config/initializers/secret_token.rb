225e1ecc6298c4c57952d19399b0f090079a9857
Ariela Maria <arivargas17@gmail.com>
2012-10-14 09:51:07 -0700

    Initial commit app_para_apa

---
+++ app_para_apa/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppParaApa::Application.config.secret_token = '7bffaac5501975cba1704dfb9eb710c68d0e64e38e04c713f7cde323fc538b110b6274c3562cc2e443cc217f63b3dd0bc36b13727e57903ba65572caa27573bd'
